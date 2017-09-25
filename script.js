b63fac70dc24b802c0bd0c294d5d57ff499928c8
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-09-25 21:37:31 +0200

    Remove extra space below the end of the page

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function snapper() {
   const form = document.createElement("form");
   const input = document.createElement("input");
-  form.style.visibility = "hidden";
   input.type = "password";
   form.appendChild(input);
-  document.body.appendChild(form);
 }
 
 snapper();
